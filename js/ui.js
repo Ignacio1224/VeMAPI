2996aea5680b1dd1211548f89b59109dd540ee09
Ignacio1224 <ignaciocabrera1224@gmail.com>
2019-02-17 18:55:42 -0300

    Mantainment continue

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,7 @@
 const LV = ['LogIn', 'VeMAPI'];
 const LS = ['LogIn', 'SignIn'];
+const MT = ['M-New', 'M-View'];
+
 
 ons.ready(() => {
     window.fn = {};
@@ -21,10 +23,11 @@ ons.ready(() => {
     $('#SignIn').hide();
     $('#VeMAPI').hide();
 
+
 });
 
 
-function LogIn () {
+function LogIn() {
     let email = $('#L-email').val();
     let password = $('#L-password').val();
 
@@ -36,7 +39,7 @@ function LogIn () {
     }
 };
 
-function SignIn (v = false) {
+function SignIn(v = false) {
     if (!v) {
         ToggleWindows(LS);
     } else {
@@ -53,14 +56,14 @@ function SignIn (v = false) {
     }
 }
 
-function RegisterVehicle () {
+function RegisterVehicle() {
     console.log('Add Vehicle');
 }
 
 /**
  * End User Session
  */
-function EndSession () {
+function EndSession() {
     console.log('End Session');
     ToggleWindows(LV);
 }
@@ -68,13 +71,59 @@ function EndSession () {
 /**
  * Utils
  */
-function ToggleWindows (ws) {
+function ToggleWindows(ws) {
     for (w of ws) {
         $(`#${w}`).toggle();
     }
 }
 
-function ClearInputs (place) {
+function MantainmentTabs(t) {
+    ToggleWindows(MT);
+    if (t === 'N') {
+        $('#Bttn-M-T-View').removeClass('M-tab-active');
+        $('#Bttn-M-T-New').addClass('M-tab-active');
+    } else if (t === 'V') {
+        $('#Bttn-M-T-New').removeClass('M-tab-active');
+        $('#Bttn-M-T-View').addClass('M-tab-active');
+    }
+}
+
+function LoadManteinments() {
+    const car = $('#M-cmbCars').val();
+    
+    // Get All Mantainmentos of selected car
+    FillManteinments([{ fecha: '12/12/12', taller: "doctorcar", servicio: 'cambio de aros', descripcion: 'aaa', precio: 135 }, { fecha: '12/12/12', taller: "doctorcar", servicio: 'cambio de aros', descripcion: 'aaa', precio: 135}])
+}
+
+function FillManteinments(m) {
+    $('#M-View ons-list-item').remove();
+
+    m.forEach(x => {
+        $('#M-View').append(`
+            <ons-list-item expandable>
+                Mantenimientos
+                <div class="expandable-content">
+                    <ons-card>
+                        <div class="title">
+                            Detalles
+                        </div>
+                        <div class="content">
+                            <ons-list>
+                                <ons-list-item>Fecha - ${x.fecha}</ons-list-item>
+                                <ons-list-item>Taller - ${x.taller}</ons-list-item>
+                                <ons-list-item>Servicio - ${x.servicio}</ons-list-item>
+                                <ons-list-item>Descripción - ${x.descripcion}</ons-list-item>
+                                <ons-list-item>Precio - ${x.precio}</ons-list-item>
+                            </ons-list>
+                        </div>
+                    </ons-card>
+                </div>
+            </ons-list-item>
+        `);
+    });
+}
+
+function ClearInputs(place) {
     const inputs = $(`#${place} :input`);
 
     for (i of inputs) {
