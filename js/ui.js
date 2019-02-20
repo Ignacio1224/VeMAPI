fa2584fa344beb6053c073e7c40e6167eecc2243
Ignacio1224 <ignaciocabrera1224@gmail.com>
2019-02-20 11:13:17 -0300

    Continuando casi finalizada ui

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,7 @@
 const LV = ['LogIn', 'VeMAPI'];
 const LS = ['LogIn', 'SignIn'];
 const MT = ['M-New', 'M-View'];
+const WT = ['W-C-Description', 'W-C-Agenda'];
 
 
 ons.ready(() => {
@@ -24,16 +25,24 @@ ons.ready(() => {
     $('#VeMAPI').hide();
 
 
+    IntroAction('L-email', LogIn, true);
+    IntroAction('L-password', LogIn, true);
+    IntroAction('S-email', LogIn, true);
+    IntroAction('S-password', LogIn, true);
+    IntroAction('S-phone', LogIn, true);
+    // IntroAction('txtSearchWorkshop', FillMap);
+
 });
 
 
 function LogIn(v = false) {
     if (!v) {
+        ClearInputs('SignIn');
         ToggleWindows(LS);
     } else {
         let email = $('#L-email').val();
         let password = $('#L-password').val();
-    
+
         if (email === 'bob' && password === 'secret') {
             ClearInputs('LogIn');
             ToggleWindows(LV);
@@ -45,6 +54,7 @@ function LogIn(v = false) {
 
 function SignIn(v = false) {
     if (!v) {
+        ClearInputs('LogIn');
         ToggleWindows(LS);
     } else {
         const sEmail = $('#S-email').val();
@@ -77,8 +87,29 @@ function EndSession() {
  */
 function ToggleWindows(ws) {
     for (w of ws) {
-        $(`#${w}`).toggle();
+        const r = Math.floor(Math.random() * 101);
+        if (r % 2 === 0) {
+            $(`#${w}`).slideToggle('slow');
+        } else if (r % 3 == 0) {
+            $(`#${w}`).toggle('slow');
+        } else if (r % 5 == 0) {
+            $(`#${w}`).toggle();
+        } else {
+            $(`#${w}`).slideToggle();
+        }
     }
+}
+
+function IntroAction(el, fn, fnArgs = null) {
+    $(`#${el}`).keyup(function (event) {
+        if (event.keyCode === 13) {
+            if (fnArgs) {
+                fn(fnArgs);
+            } else {
+                fn();
+            }
+        }
+    });
 }
 
 function MantainmentTabs(t) {
@@ -96,11 +127,60 @@ function MantainmentTabs(t) {
     }
 }
 
+function WorkshopTabs(t) {
+    if (t !== 'R') {
+        ToggleWindows(WT);
+    } else {
+        console.log('Marcar Ruta');
+        
+    }
+    switch (t) {
+        case 'D':
+            $('#Bttn-W-T-Agenda').removeClass('three-tab-active');
+            $('#Bttn-W-T-Route').removeClass('three-tab-active');
+            $('#Bttn-W-T-Description').addClass('three-tab-active');
+            $('#Bttn-W-T-Description').prop("disabled", true);
+            $('#Bttn-W-T-Agenda').prop("disabled", false);
+            $('#Bttn-W-T-Route').prop("disabled", false);
+            break;
+        case 'A':
+            $('#Bttn-W-T-Description').removeClass('three-tab-active');
+            $('#Bttn-W-T-Route').removeClass('three-tab-active');
+            $('#Bttn-W-T-Agenda').addClass('three-tab-active');
+            $('#Bttn-W-T-Agenda').prop("disabled", true);
+            $('#Bttn-W-T-Description').prop("disabled", false);
+            $('#Bttn-W-T-Route').prop("disabled", false);
+            break;
+        case 'R':
+            $('#Bttn-W-T-Description').removeClass('three-tab-active');
+            $('#Bttn-W-T-Agenda').removeClass('three-tab-active');
+            $('#Bttn-W-T-Route').addClass('three-tab-active');
+            $('#Bttn-W-T-Route').prop("disabled", true);
+            $('#Bttn-W-T-Description').prop("disabled", false);
+            $('#Bttn-W-T-Agenda').prop("disabled", false);
+            break;
+        default:
+            break;
+    }
+}
+
 function LoadManteinments() {
     const car = $('#M-cmbCars').val();
-    
+
     // Get All Mantainmentos of selected car
-    FillManteinments([{ fecha: '12/12/12', taller: "doctorcar", servicio: 'cambio de aros', descripcion: 'aaa', precio: 135 }, { fecha: '12/12/12', taller: "doctorcar", servicio: 'cambio de aros', descripcion: 'aaa', precio: 135}])
+    FillManteinments([{
+        fecha: '12/12/12',
+        taller: "doctorcar",
+        servicio: 'cambio de aros',
+        descripcion: 'aaa',
+        precio: 135
+    }, {
+        fecha: '12/12/12',
+        taller: "doctorcar",
+        servicio: 'cambio de aros',
+        descripcion: 'aaa',
+        precio: 135
+    }])
 }
 
 function FillManteinments(m) {
@@ -144,16 +224,25 @@ function ShowModal(value) {
 
     $('#modalSpan').html(value);
 
-    modal.show();
+    modal.show('fold');
     setTimeout(function () {
-        modal.hide();
-    }, 1800);
+        modal.hide('fold');
+    }, 2000);
 }
 
+
+function FillMap() {
+    const search = $('#txtSearchWorkshop').val();
+
+    console.log("Call ajax");
+}
 // Google Maps API
 function initMap() {
     map = new google.maps.Map(document.getElementById('googleMap'), {
-        center: { lat: -34.397, lng: 150.644 },
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
         zoom: 8
     });
 }
